7e4e7936431c1a02a42bae1c0ecb79c73eb06354
currymich <currymich@gmail.com>
2016-12-22 14:59:06 -0800

    linking correct JS file

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBI6sUQ6PCxQrvWGpAp8TKsO9FYi_iULCI",
+    authDomain: "connect-four-780e9.firebaseapp.com",
+    databaseURL: "https://connect-four-780e9.firebaseio.com",
+    storageBucket: "connect-four-780e9.appspot.com",
+    messagingSenderId: "857718574300"
+  };
+  firebase.initializeApp(config);
+</script>
